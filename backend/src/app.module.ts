dddd35de19bdfb71334c39886255eb1d99a0e959
Doyage <pcstt@naver.com>
2024-12-30 23:39:40 +0900

    feat: add-redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,46 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { jwtConstants } from './auth/constants';
 import dataSource from './config/dataSource';
 import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      cache: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: () => dataSource.options,
     }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      ttl: parseInt(process.env.REDIS_TTL),
+    }),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
     UserModule,
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, AuthService],
+  exports: [CacheModule, JwtModule],
 })
 export class AppModule {}
