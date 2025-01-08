a250f870e4f4b3a512d862a4a8cfe8a5d385460b
Doyage <pcstt@naver.com>
2025-01-08 23:35:26 +0900

    feat: sync redis and db

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -2,22 +2,55 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cache } from 'cache-manager';
+import { Hobby } from 'src/hobby/entities/hobby.entity';
+import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Chat } from './entities/chat.entity';
 
 @Injectable()
 export class ChatService {
   constructor(
-    @InjectRepository(Chat)
-    private readonly chatRepository: Repository<Chat>,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @InjectRepository(Chat) private readonly chatRepository: Repository<Chat>,
+    @InjectRepository(Hobby)
+    private readonly hobbyRepository: Repository<Hobby>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
+
+  private isFiveMinutesPassed(
+    lastTimestamp: number,
+    currentTimestamp: number,
+  ): boolean {
+    const elapsed = currentTimestamp - lastTimestamp;
+    return elapsed >= 5 * 60 * 1000;
+  }
 
   async saveMessageToRedis(roomId: string, message: any): Promise<void> {
     const key = `chat:${roomId}`;
-    const msg = JSON.stringify(message);
+    const timestamp = Date.now();
+    const msg = { message, timestamp };
 
-    await this.cacheManager.set(key, msg, { ttl: 24 * 60 * 60 });
+    let cachedMessages = await this.cacheManager.get(key);
+
+    if (cachedMessages) {
+      cachedMessages = JSON.parse(cachedMessages);
+
+      cachedMessages.push(msg);
+    } else {
+      cachedMessages = [msg];
+    }
+
+    await this.cacheManager.set(key, JSON.stringify(cachedMessages), {
+      ttl: 24 * 60 * 60,
+    });
+
+    if (
+      cachedMessages.length >= 5 ||
+      this.isFiveMinutesPassed(cachedMessages[0]?.timestamp, timestamp)
+    ) {
+      await this.flushRedisToDatabase(roomId);
+      cachedMessages = [];
+    }
   }
 
   async flushRedisToDatabase(roomId: string): Promise<void> {
@@ -26,11 +59,32 @@ export class ChatService {
 
     if (messages) {
       const parsedMessages = JSON.parse(messages);
-      const chatMessages = parsedMessages.map((msg) =>
-        this.chatRepository.create(msg),
+
+      const chatMessages = await Promise.all(
+        parsedMessages.map(async (msg) => {
+          const { message } = msg;
+          const hobby = await this.hobbyRepository.findOne({
+            where: { id: +roomId },
+          });
+          const author = await this.userRepository.findOne({
+            where: { id: message.userId },
+          });
+
+          if (!hobby || !author) {
+            throw new Error('Invalid roomId or userId');
+          }
+
+          return this.chatRepository.create({
+            hobby,
+            author,
+            message: message.message,
+            createdAt: new Date(message.createdAt),
+          });
+        }),
       );
 
       await this.chatRepository.save(chatMessages);
+
       await this.cacheManager.del(key);
     }
   }
