41c754b85397271128e03df5840462266a03cc65
Doyage <pcstt@naver.com>
2025-01-08 23:35:26 +0900

    feat: sync redis and db

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hobby } from 'src/hobby/entities/hobby.entity';
+import { User } from 'src/user/entities/user.entity';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { Chat } from './entities/chat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat])],
+  imports: [TypeOrmModule.forFeature([Chat, Hobby, User])],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
